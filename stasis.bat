60e43536dc5251aeb5146dafbea2356e2118d9a4
Ben Maddox <benwmaddox@gmail.com>
2025-12-08 08:24:55 -0500

    chore: skip dotnet build on stasis test

--- stasis.bat
+++ stasis.bat
@@ -1,6 +1,10 @@
 @echo off
 setlocal
 set "SCRIPT_DIR=%~dp0"
-dotnet run --no-restore --project "%SCRIPT_DIR%Stasis.Cli\Stasis.Cli.csproj" -- %*
+set "DOTNET_ARGS=--no-restore"
+if "%1"=="test" (
+    set "DOTNET_ARGS=--no-build"
+)
+dotnet run %DOTNET_ARGS% --project "%SCRIPT_DIR%Stasis.Cli\Stasis.Cli.csproj" -- %*
 set "CODE=%ERRORLEVEL%"
 endlocal & exit /b %CODE%
