a935c0f88b6b2cbec6d3b27849da0dcec289216c
Ben Maddox <benwmaddox@gmail.com>
2025-12-19 07:39:08 -0500

    chore: add backend timing block

--- test.bat
+++ test.bat
@@ -14,4 +14,20 @@ if errorlevel 1 exit /b 1
 dotnet run --project Stasis.Cli -- test samples\fib_tests.stasis --backend cranelift
 if errorlevel 1 exit /b 1
 
+powershell -NoProfile -Command ^
+  "$samples = @('samples\\tests.stasis','samples\\fib_tests.stasis','samples\\strings.stasis');" ^
+  "$env:STASIS_SUPPRESS_WARNINGS='1';" ^
+  "$build = Measure-Command { & .\\build.bat | Out-Host };" ^
+  "$llvm = Measure-Command { foreach ($s in $samples) { & dotnet run --project Stasis.Cli -- test $s --backend llvm | Out-Host } };" ^
+  "$cranelift = Measure-Command { foreach ($s in $samples) { & dotnet run --project Stasis.Cli -- test $s --backend cranelift | Out-Host } };" ^
+  "$buildMs = [math]::Round($build.TotalMilliseconds, 0);" ^
+  "$llvmMs = [math]::Round($llvm.TotalMilliseconds, 0);" ^
+  "$craneliftMs = [math]::Round($cranelift.TotalMilliseconds, 0);" ^
+  "Write-Host ('Build ms=' + $buildMs);" ^
+  "Write-Host ('LLVM subset ms=' + $llvmMs);" ^
+  "Write-Host ('Cranelift subset ms=' + $craneliftMs);" ^
+  "Write-Host ('LLVM total ms=' + ($buildMs + $llvmMs));" ^
+  "Write-Host ('Cranelift total ms=' + ($buildMs + $craneliftMs));"
+if errorlevel 1 exit /b 1
+
 endlocal
