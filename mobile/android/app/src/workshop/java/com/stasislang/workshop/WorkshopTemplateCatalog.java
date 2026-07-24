e39ceed7b7f014e21c588fe6d211be2ffc77da8e
Ben Maddox <benwmaddox@gmail.com>
2026-07-24 12:54:34 -0400

    docs: add theory-building practice

--- mobile/android/app/src/workshop/java/com/stasislang/workshop/WorkshopTemplateCatalog.java
+++ mobile/android/app/src/workshop/java/com/stasislang/workshop/WorkshopTemplateCatalog.java
@@ -25,6 +25,7 @@ final class WorkshopTemplateCatalog {
             },
             new String[] { "tests/enemy_paddle_speed_schedule.test.stasis" },
             new String[] {
+                    "AGENTS.md",
                     "assets/manifest.json",
                     "assets/ball.svg",
                     "assets/paddle.svg",
@@ -57,6 +58,7 @@ final class WorkshopTemplateCatalog {
             },
             new String[] { "tests/exploration_gameplay.test.stasis" },
             new String[] {
+                    "AGENTS.md",
                     "assets/manifest.json",
                     "assets/player.svg",
                     "assets/sun_keepsake.svg",
